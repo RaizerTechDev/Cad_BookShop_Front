518f4379cbf57c48ba74a9e187231070497461ed
Rafael Raizer <87991807+RafaRz76Dev@users.noreply.github.com>
2023-07-12 17:06:31 -0300

    Update styles.js
    🚀 Realizando alterações de códigos no projeto com as seguintes atualizações:
    
    ✔ Foto WhatsApp mais inovadora e quando clica com o mouse vai ser encaminhado para falar diretamente comigo.
    
    ✔ Na foto perfil a modificação foi acrescentar um <a href="https://portifolio-rafarz76dev.netlify.app/"> e ao clicar com o mouse, o #usuário, o #recrutador ou a #empresa será direcionado para estar interagindo com o my new portfólio.

--- src/components/Header/styles.js
+++ src/components/Header/styles.js
@@ -1,38 +1,50 @@
 import styled from "styled-components";
 
+export const Container = styled.div`
+    background: var(--dark-400);
+`;
+
 export const Content = styled.div`
-background: #27272A;
-max-width: 1440px;
-margin: 0 auto;
-margin-left: 2rem;
-padding: 0 3rem;
-align-items: end;
-justify-content: space-between;
-margin-top: 2rem;  
-display: flex;
+    max-width: 1440px;
+    margin: 0 auto;
+    
+    padding: 1rem 5rem;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    
 
     button {
-        background: #27272A;
-        width: 2rem;
-        height:3rem;
+        border: 2;
+        border-radius: 50%;
+        overflow: hidden;
+        width: 3rem;
+        height: 3rem;
+        background: var(--dark-300);
         display: flex;
         justify-content: center;
         align-items: center;
         transition: filter 0.2s;
-        margin-top: 2rem;
         
-      img{
-        width: 4rem;
-        height: 3rem;
-        margin-top: -1rem;  
-        margin-left: -2rem; 
-        border-radius: 40%;
-                 
         &:hover{
             filter: brightness(0.9);
-        }           
-    }
+        }
 
+        img {
+            width: 4rem;
+            height: 3rem;
+            margin-top: 0rem;  
+            margin-left: -1rem; 
+            border-radius: 40%;
+        }
+    }
 `;
 
+export const ImageLogo = styled.img`
+    padding: 5px 8px;
+    background: var(--dark-300);
+    border-radius: 8px;
+    margin-left: -2rem;   
+    }  
+`;
 
