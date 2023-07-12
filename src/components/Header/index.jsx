45b4af7fe52f1248bd9b9b6f31aa37b8a452112c
Rafael Raizer <87991807+RafaRz76Dev@users.noreply.github.com>
2023-07-12 17:04:04 -0300

    Update index.jsx
    🚀 Realizando alterações de códigos no projeto:
    	
    ✔Foto WhatsApp mais inovadora e quando clica com o mouse vai ser encaminhado para falar diretamente comigo.
    
    ✔Na foto perfil a modificação foi acrescentar um <a href="https://portifolio-rafarz76dev.netlify.app/"> e ao clicar com o mouse, o #usuário, o #recrutador ou a #empresa será direcionado para estar interagindo com o my new portfólio.

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,19 +1,28 @@
-/* eslint-disable jsx-a11y/alt-text */
-import { Content } from "./styles";
+import {
+  Container,
+  Content,
+  ImageLogo,
+} from './styles';
+import whatsSvg from '../../assets/icons/whatsapp.svg';
 
-export function Header() {
+export function Header(){
   return (
-    <Content>
-      <button>
-        <a href="https://api.whatsapp.com/send/?phone=47999327137">
-          <img
-            src="https://img.shields.io/badge/WhatsApp-25D366?style=for-the-badge&logo=whatsapp&logoColor=white"
-            target="-blank"
-          />
-        </a>
-      </button>
+      <Container>
+          <Content>
+          <a href="https://api.whatsapp.com/send/?phone=47999327137">  
+          <button>            
+              <ImageLogo src={whatsSvg} alt="stackX" />
+              </button>
+            </a> 
 
-      <img src="https://github.com/RafaRz76Dev.png" alt="admin" />
-    </Content>
+            <a href="https://portifolio-rafarz76dev.netlify.app/">  
+              <button>
+              <img  src="https://github.com/RafaRz76Dev.png" />
+              </button>
+              </a>
+
+              
+          </Content>
+      </Container>
   );
 }
